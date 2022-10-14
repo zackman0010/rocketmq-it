8b3cb0002a16f32ed58468cfdc66567d725fb4c0
凌楚 <yangkun.ayk@alibaba-inc.com>
2022-10-14 10:54:04 +0800

    WIP

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,12 @@
 package org.example;
 
 import org.apache.rocketmq.broker.BrokerController;
+import org.apache.rocketmq.client.ClientConfig;
+import org.apache.rocketmq.client.apis.ClientConfiguration;
+import org.apache.rocketmq.client.apis.ClientServiceProvider;
+import org.apache.rocketmq.client.apis.message.Message;
+import org.apache.rocketmq.client.apis.producer.Producer;
+import org.apache.rocketmq.client.apis.producer.SendReceipt;
 import org.apache.rocketmq.common.MQVersion;
 import org.apache.rocketmq.namesrv.NamesrvController;
 import org.apache.rocketmq.proxy.grpc.v2.GrpcMessagingApplication;
@@ -42,6 +48,17 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         final Main main = new Main();
+
+        IntegrationTestBase.initTopic("aa", main.nsAddr, main.clusterName);
+        final ClientServiceProvider provider = ClientServiceProvider.loadService();
+        String endpoint = "localhost:8099";
+        final ClientConfiguration clientConfiguration = ClientConfiguration.newBuilder().setEndpoints(endpoint).build();
+        final Producer producer =
+            provider.newProducerBuilder().setClientConfiguration(clientConfiguration).setTopics("aa").build();
+        final Message message = provider.newMessageBuilder().setTopic("aa").setBody("hello".getBytes()).build();
+        final SendReceipt sendReceipt = producer.send(message);
+        System.out.println(sendReceipt);
+        System.out.println("xx");
         main.shutdown();
         System.out.println("done");
     }
